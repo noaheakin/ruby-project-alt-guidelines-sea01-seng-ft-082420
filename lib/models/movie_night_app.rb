8980ce46f6125967d6bb26262ad39da0b1293dd1
noaheakin <noaheakin@gmail.com>
2020-09-09 12:13:21 -0700

    added make_a_choice method in movie_night_app.rb

--- lib/models/movie_night_app.rb
+++ lib/models/movie_night_app.rb
@@ -19,7 +19,7 @@ class MovieNightApp
 
     def genre_query
         puts "What genre are you interested in?"
-        @genres.each_with_index {|value, key| puts "#{key+=1}. #{value}"}
+        @genres.each_with_index {|value, key| puts "(#{key+=1}) #{value}"}
         user_genre_input = gets.chomp.to_i
         user_genre = @genres[user_genre_input -= 1]
         genre_movies = Movie.all.select {|movie| movie.genre == user_genre}
@@ -28,14 +28,27 @@ class MovieNightApp
 
     def runtime_query
         puts "How long do you have to watch a movie?"
-        @runtime.each_with_index {|value, key| puts "#{key+=1}. #{value}"}    
+        @runtime.each_with_index {|value, key| puts "(#{key+=1}) #{value}"}    
         user_runtime_input = gets.chomp.to_i
         user_runtime = @runtime[user_runtime_input -= 1]
         runtime_movies = @genres_array.select {|movie| movie.runtime == user_runtime}
         @genres_and_runtimes_array += runtime_movies
-        puts @genres_and_runtimes_array.map {|movie| movie.title}
     end
 
+    def make_a_choice
+        puts "Please select a movie from the following:"
+        options_array = @genres_and_runtimes_array.map {|movie| movie.title}
+        options_array << "*** Pick for me! ***"
+        final_options = options_array.each_with_index {|value, key| puts "(#{key+=1}) #{value}"}
+        user_choice_input = gets.chomp.to_i
+        user_choice = final_options[user_choice_input -= 1]
+        if user_choice == "*** Pick for me! ***"
+            puts "We've got you! Enjoy #{@genres_and_runtimes_array.sample.title}!"  
+        else 
+            puts "Great choice! We hope you enjoy #{user_choice}!"
+        end
+    end
+    
     # def release_year_query
     #     puts "When would you like your movie to have been released?"
     #     user_release_year = gets.chomp
@@ -45,6 +58,7 @@ class MovieNightApp
         welcome
         genre_query
         runtime_query
+        make_a_choice
         # release_year_query
     end
 
