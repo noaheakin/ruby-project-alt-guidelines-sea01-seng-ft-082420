a558074919ca11a84f088d03fa1e754a6b6b9ead
noaheakin <noaheakin@gmail.com>
2020-09-10 11:34:03 -0700

    changed all seeds to .create

--- db/seeds.rb
+++ db/seeds.rb
@@ -198,65 +198,65 @@ batman_begins = Movie.create(title: "Batman Begins", genre: "Action/Adventure", 
 
 green_book = Movie.create(title: "Green Book", genre: "Drama", runtime: "2 - 2.5 hours")
 
-the_dark_knight_rises = Movie.new(title: "The Dark Knight Rises", genre: "Action/Adventure", runtime: "over 2.5 hours")
+the_dark_knight_rises = Movie.create(title: "The Dark Knight Rises", genre: "Action/Adventure", runtime: "over 2.5 hours")
 
-once_upon_a_time_in_america = Movie.new(title: "Once Upon a Time in America", genre: "Drama", runtime: "over 2.5 hours")
+once_upon_a_time_in_america = Movie.create(title: "Once Upon a Time in America", genre: "Drama", runtime: "over 2.5 hours")
 
-aliens = Movie.new(title: "Aliens", genre: "Sci-Fi", runtime: "2 - 2.5 hours")
+aliens = Movie.create(title: "Aliens", genre: "Sci-Fi", runtime: "2 - 2.5 hours")
 
-avengers_endgame = Movie.new(title: "Avengers: Endgame", genre: "Action/Adventure", runtime: "over 2.5 hours")
+avengers_endgame = Movie.create(title: "Avengers: Endgame", genre: "Action/Adventure", runtime: "over 2.5 hours")
 
-your_name = Movie.new(title: "Your Name.", genre: "Drama", runtime: "1.5 - 2 hours")
+your_name = Movie.create(title: "Your Name.", genre: "Drama", runtime: "1.5 - 2 hours")
 
-coco = Movie.new(title: "Coco", genre: "Family", runtime: "1.5 - 2 hours")
+coco = Movie.create(title: "Coco", genre: "Family", runtime: "1.5 - 2 hours")
 
-american_beauty = Movie.new(title: "American Beauty", genre: "Drama", runtime: "2 - 2.5 hours")
+american_beauty = Movie.create(title: "American Beauty", genre: "Drama", runtime: "2 - 2.5 hours")
 
-braveheart = Movie.new(title: "Braveheart", genre: "Drama", runtime: "over 2.5 hours")
+braveheart = Movie.create(title: "Braveheart", genre: "Drama", runtime: "over 2.5 hours")
 
-three_idiots = Movie.new(title: "3 idiots", genre: "Comedy", runtime: "over 2.5 hours")
+three_idiots = Movie.create(title: "3 idiots", genre: "Comedy", runtime: "over 2.5 hours")
 
-das_boot = Movie.new(title: "Das Boot", genre: "Thriller", runtime: "2 - 2.5 hours")
+das_boot = Movie.create(title: "Das Boot", genre: "Thriller", runtime: "2 - 2.5 hours")
 
-toy_story = Movie.new(title: "Toy Story", genre: "Action/Adventure", runtime: "1 - 1.5 hours")
+toy_story = Movie.create(title: "Toy Story", genre: "Action/Adventure", runtime: "1 - 1.5 hours")
 
-high_and_low = Movie.new(title: "High and Low", genre: "Drama", runtime: "2 - 2.5 hours")
+high_and_low = Movie.create(title: "High and Low", genre: "Drama", runtime: "2 - 2.5 hours")
 
-amadeus = Movie.new(title: "Amadeus", genre: "Drama", runtime: "over 2.5 hours")
+amadeus = Movie.create(title: "Amadeus", genre: "Drama", runtime: "over 2.5 hours")
 
-like_stars_on_earth = Movie.new(title: "Like Stars on Earth", genre: "Family", runtime: "over 2.5 hours")
+like_stars_on_earth = Movie.create(title: "Like Stars on Earth", genre: "Family", runtime: "over 2.5 hours")
 
-star_wars_return_of_the_jedi = Movie.new(title: "Star Wars: Episode VI - Return of the Jedi", genre: "Fantasy", runtime: "2 - 2.5 hours")
+star_wars_return_of_the_jedi = Movie.create(title: "Star Wars: Episode VI - Return of the Jedi", genre: "Fantasy", runtime: "2 - 2.5 hours")
 
-inglourious_basterds = Movie.new(title: "Inglourious Basterds", genre: "Action/Adventure", runtime: "over 2.5 hours")
+inglourious_basterds = Movie.create(title: "Inglourious Basterds", genre: "Action/Adventure", runtime: "over 2.5 hours")
 
-reservoir_dogs = Movie.new(title: "Reservoir Dogs", genre: "1 - 1.5 hours", runtime: "Drama")
+reservoir_dogs = Movie.create(title: "Reservoir Dogs", genre: "1 - 1.5 hours", runtime: "Drama")
 
-capernaum = Movie.new(title: "Capernaum", genre: "Drama", runtime: "2 - 2.5 hours")
+capernaum = Movie.create(title: "Capernaum", genre: "Drama", runtime: "2 - 2.5 hours")
 
-good_will_hunting = Movie.new(title: "Good Will Hunting", genre: "Romance", runtime: "2 - 2.5 hours")
+good_will_hunting = Movie.create(title: "Good Will Hunting", genre: "Romance", runtime: "2 - 2.5 hours")
 
-twothousandone_a_space_odyssey = Movie.new(title: "2001: A Space Odyssey", genre: "Sci-Fi", runtime: "2 - 2.5 hours")
+twothousandone_a_space_odyssey = Movie.create(title: "2001: A Space Odyssey", genre: "Sci-Fi", runtime: "2 - 2.5 hours")
 
-requiem_for_a_dream = Movie.new(title: "Requiem for a Dream", genre: "Drama", runtime: "1.5 - 2 hours")
+requiem_for_a_dream = Movie.create(title: "Requiem for a Dream", genre: "Drama", runtime: "1.5 - 2 hours")
 
-vertigo = Movie.new(title: "Vertigo", genre: "Romance", runtime: "2 - 2.5 hours")
+vertigo = Movie.create(title: "Vertigo", genre: "Romance", runtime: "2 - 2.5 hours")
 
-m = Movie.new(title: "M", genre: "Thriller", runtime: "1.5 - 2 hours")
+m = Movie.create(title: "M", genre: "Thriller", runtime: "1.5 - 2 hours")
 
-dangal = Movie.new(title: "Dangal", genre: "Action/Adventure", runtime: "over 2.5 hours")
+dangal = Movie.create(title: "Dangal", genre: "Action/Adventure", runtime: "over 2.5 hours")
 
-eternal_sunshine_of_the_spotless_mind = Movie.new(title: "Eternal Sunshine of the Spotless Mind", genre: "Romance", runtime: "1.5 - 2 hours")
+eternal_sunshine_of_the_spotless_mind = Movie.create(title: "Eternal Sunshine of the Spotless Mind", genre: "Romance", runtime: "1.5 - 2 hours")
 
-nineteenseventeen = Movie.new(title: "1917", genre: "War", runtime: "1.5 - 2 hours")
+nineteenseventeen = Movie.create(title: "1917", genre: "War", runtime: "1.5 - 2 hours")
 
-the_hunt = Movie.new(title: "The Hunt", genre: "Drama", runtime: "1 - 1.5 hours")
+the_hunt = Movie.create(title: "The Hunt", genre: "Drama", runtime: "1 - 1.5 hours")
 
-citizen_kane = Movie.new(title: "Citizen Kane", genre: "Drama", runtime: "1.5 - 2 hours")
+citizen_kane = Movie.create(title: "Citizen Kane", genre: "Drama", runtime: "1.5 - 2 hours")
 
-full_metal_jacket = Movie.new(title: "1.5 - 2 hours", genre: "War", runtime: "1.5 - 2 hours")
+full_metal_jacket = Movie.create(title: "1.5 - 2 hours", genre: "War", runtime: "1.5 - 2 hours")
 
-bicycle_thieves = Movie.new(title: "Bicycle Thieves", genre: "Drama", runtime: "1 - 1.5 hours")
+bicycle_thieves = Movie.create(title: "Bicycle Thieves", genre: "Drama", runtime: "1 - 1.5 hours")
 
 the_wizard_of_oz = Movie.create(title: "The Wizard of Oz", genre: "Fantasy", runtime: "1.5 - 2 hours")
 
